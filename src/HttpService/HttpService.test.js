f4937c15d67b61204da55ba1adb746e8e76c4db9
pmangialavori <pasquale.mangialavor@edreamsodigeo.com>
2019-10-02 22:38:15 +0200

    npm test on pre-commit

--- src/HttpService/HttpService.test.js
+++ src/HttpService/HttpService.test.js
@@ -3,6 +3,7 @@ import HttpService from './index.js';
 let httpService = null;
 beforeEach(() => {
   httpService = new HttpService();
+  fetch.mockResponseOnce(JSON.stringify({data: '12345'}));
 });
 
 test('simple test', async () => {
