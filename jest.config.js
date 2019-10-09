a1d5a04653752b9460f2f246e7236800127211da
pmangialavori <pasquale.mangialavor@edreamsodigeo.com>
2019-10-09 11:45:36 +0200

    Jest transform in package.json

--- jest.config.js
+++ jest.config.js
@@ -170,7 +170,7 @@ module.exports = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    '\\.m?jsx?$': 'esm',
+    '\\.m?jsx?$': 'jest-esm-transformer',
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
