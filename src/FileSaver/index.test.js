7125e8b4e920f0774fa3e131d9c6fa4e610805af
Pasquale Mangialavori <pasquale.mangialavor@edreamsodigeo.com>
2020-02-18 17:07:17 +0100

    add tests for FileSaver

--- src/FileSaver/index.test.js
+++ src/FileSaver/index.test.js
@@ -13,3 +13,15 @@ test('should generate Blob', () => {
   expect(blob).toBeDefined();
   expect(blob instanceof Blob);
 });
+
+
+test('should download the file', () => {
+  HTMLAnchorElement.prototype.click = jest.fn();
+  const output = download({url: URL.createObjectURL('mock')});
+  expect(output).toBeDefined();
+  expect(output instanceof HTMLAnchorElement).toBe(true);
+  expect(output.download).toContain('report');
+  expect(output.download).toContain('txt');
+  expect(HTMLAnchorElement.prototype.click).toHaveBeenCalledTimes(1);
+  expect(output.style.display).toEqual('none');
+});
